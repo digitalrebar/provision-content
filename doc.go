d81d0b4531022760040ded45de0e2b2e8d981352
Greg Althaus <galthaus@austin.rr.com>
2017-09-12 10:31:44 -0500

    Add publish command to push content to cloud

---
+++ doc.go
@@ -0,0 +1,4 @@
+// DigitalRebar Provision is a provisioning binary that provides a
+// DHCP server and Provisioner system that allows for simply
+// booting and installing bare metal.
+package provision_content
