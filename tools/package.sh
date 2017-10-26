0808664aeddf2bd55cfa199a9141e2c9910e829b
Greg Althaus <galthaus@austin.rr.com>
2017-10-26 11:09:27 -0500

    First move things into the two new parts.
    Next up renaming

--- tools/package.sh
+++ tools/package.sh
@@ -8,14 +8,10 @@ if [[ $(uname -m) == x86_64 ]] ; then
 fi
 case $(uname -s) in
     Darwin)
-        binpath="bin/darwin/$family"
         shasum="command shasum -a 256"
-        tar="command bsdtar"
         ;;
     Linux)
-        binpath="bin/linux/$family"
         shasum="command sha256sum"
-        tar="command bsdtar"
         ;;
     *)
         # Someday, support installing on Windows.  Service creation could be tricky.
@@ -23,24 +19,14 @@ case $(uname -s) in
         exit 1;;
 esac
 
-if [ ! -e drp ] ; then
-  mkdir -p drp
-  cd drp
-
-  DRP_VERSION=tip
-  echo "Installing Version $DRP_VERSION of Digital Rebar Provision"
-  curl -sfL -o dr-provision.zip https://github.com/digitalrebar/provision/releases/download/$DRP_VERSION/dr-provision.zip
-  curl -sfL -o dr-provision.sha256 https://github.com/digitalrebar/provision/releases/download/$DRP_VERSION/dr-provision.sha256
-  $shasum -c dr-provision.sha256
-  $tar -xf dr-provision.zip
-
-  rm -f drpcli
-  ln -s $binpath/drpcli drpcli
-
-  cd ..
-fi
+go get -u github.com/digitalrebar/provision/cmds/drbundler
+PATH=$PATH:$GOPATH/bin
 
 . tools/version.sh
 
-drp/drpcli contents bundle drp-community-content.yaml Description="Digital Rebar Provision Community Content" Version="$Prepart$MajorV.$MinorV.$PatchV$Extra-$GITHASH" Source="https://github.com/digitalrebar/provision-content" Name="drp-community-content" --format=yaml
-$shasum drp-community-content.yaml > drp-community-content.sha256
+for dir in content contrib ; do
+    echo -n "$Prepart$MajorV.$MinorV.$PatchV$Extra-$GITHASH" > $dir/._Version.meta
+    drbundler $dir drp-community-$dir.yaml
+    $shasum drp-community-$dir.yaml > drp-community-$dir.sha256
+done
+
