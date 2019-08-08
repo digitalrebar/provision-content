75cc43908d5a88af50346f428408c93fc8e490a6
Greg Althaus <galthaus@austin.rr.com>
2019-08-07 20:32:28 -0500

    Fix notify script for v4 changes

--- tools/notify.sh
+++ tools/notify.sh
@@ -2,8 +2,13 @@
 
 repo=$1
 
-if [ "${TRAVIS_BRANCH}" = "tip" ] || [ "${TRAVIS_BRANCH}" = "stable" ]
+if [ "${TRAVIS_TAG}" != "" ]
 then
-    curl -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token ${TRAVIS_API_TOKEN}" "https://api.travis-ci.com/repo/rackn%2Frackn-saas/requests" -X POST -d "{ \"request\": { \"message\": \"Trigger build by ${repo}\", \"branch\": \"${TRAVIS_BRANCH}\" } }"
+    curl -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token ${TRAVIS_API_TOKEN}" "https://api.travis-ci.com/repo/rackn%2Frackn-saas/requests" -X POST -d "{ \"request\": { \"message\": \"Trigger build by ${repo}\", \"branch\": \"stable\" } }"
+fi
+
+if [ "${TRAVIS_BRANCH}" = "v4" ]
+then
+    curl -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token ${TRAVIS_API_TOKEN}" "https://api.travis-ci.com/repo/rackn%2Frackn-saas/requests" -X POST -d "{ \"request\": { \"message\": \"Trigger build by ${repo}\", \"branch\": \"tip\" } }"
 fi
 
