cf443a14cd971cc1025a52827b21c3dc2951bf83
Greg Althaus <galthaus@austin.rr.com>
2019-09-27 13:02:06 -0500

    Add flexiflow to published pieces

--- tools/pieces.sh
+++ tools/pieces.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in validation burnin classify-tests classify coreos dell-support dev-library drp-prom-mon flash hardware-tooling hpe-support image-builder kubespray lenovo-support opsramp os-other rancheros task-library terraform drp-community-content drp-community-contrib krib sledgehammer-builder ; do
+for i in validation burnin classify-tests classify coreos dell-support dev-library drp-prom-mon flash hardware-tooling hpe-support image-builder kubespray lenovo-support opsramp os-other rancheros task-library terraform drp-community-content drp-community-contrib krib sledgehammer-builder flexiflow ; do
     echo $i
 done
 
