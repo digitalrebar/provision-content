367b2a687a176c0609d56c2a19fb24ae611649b8
Shane Gibson <shane@rackn.com>
2020-02-05 22:02:53 -0800

    Remove generic runner, fix fake machine mappings script

--- vmware-lib/tools/drpcli-create-machines.sh
+++ vmware-lib/tools/drpcli-create-machines.sh
@@ -8,5 +8,6 @@
 #  These are used to launch workflow contexts without using a real machine.
 ###
 
-drpcli machines create  '{"Name": "zz-runner-1", "Meta": {"BaseContext": "runner-1"}}'
-drpcli machines create  '{"Name": "zz-runner-2", "Meta": {"BaseContext": "runner-2"}}'
+# prefix w/ 'zz' to sort last - and create 2 for now
+drpcli machines create  '{"Name": "zz-govc-1", "Meta": {"BaseContext": "govc"}'
+drpcli machines create  '{"Name": "zz-govc-2", "Meta": {"BaseContext": "govc"}'
