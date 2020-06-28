d7fa6ffd32287c87882440419ef335480e29808b
Shane Gibson <shane@rackn.com>
2020-06-28 14:52:39 -0700

    feat(packer-builder): fix packer binary version

--- packer-builder/packer/setup-tools.sh
+++ packer-builder/packer/setup-tools.sh
@@ -4,7 +4,11 @@
 
 set -e
 
-PKR_VER="1.6.0"
+############ WARNING
+############ WARNING - packer 1.6.0 seems to have changed JSON format - verify
+############ WARNING - JSON files by running 1.6.0 version  'packer fix <FILE>.json'
+############ WARNING
+PKR_VER="1.5.6"
 PPW_VER="v0.9.0"
 VGT_VER="2.2.9"
 
